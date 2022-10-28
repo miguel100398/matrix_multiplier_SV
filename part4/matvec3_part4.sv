f5c39c170655b2c0facfc0ce842d0ea5e0ee3ab8
miguel100398 <miguel_angel_bucio@hotmail.com>
2022-10-28 01:55:08 -0500

    Part 4 added

---
+++ part4/matvec3_part4.sv
@@ -0,0 +1,45 @@
+module matvec3_part4(clk, reset, input_valid, input_ready,
+                    input_data, output_valid, output_ready, output_data, new_matrix);
+
+    input   logic clk, reset, input_valid, output_ready, new_matrix;
+    input   logic signed [13:0] input_data;
+    output  logic signed [27:0] output_data;
+    output  logic output_valid, input_ready;
+
+    logic [5:0] addr_w;
+    logic [2:0] addr_x;
+    logic wr_en_x;
+    logic wr_en_w;
+    logic clear_acc;
+    logic en_acc;
+
+    control_part4 control(
+        .clk(clk),
+        .rst(reset),
+        .input_valid(input_valid),
+        .output_ready(output_ready),
+        .new_matrix(new_matrix),
+        .addr_x(addr_x),
+        .wr_en_x(wr_en_x),
+        .addr_w(addr_w),
+        .wr_en_w(wr_en_w),
+        .clear_acc(clear_acc),
+        .en_acc(en_acc),
+        .input_ready(input_ready),
+        .output_valid(output_valid)
+    );
+
+    datapath_part4 datapath(
+        .clk(clk),
+        .rst(reset),
+        .input_data(input_data),
+        .addr_x(addr_x),
+        .wr_en_x(wr_en_x),
+        .addr_w(addr_w),
+        .wr_en_w(wr_en_w),
+        .clear_acc(clear_acc),
+        .en_acc(en_acc),
+        .output_data(output_data)
+    );
+
+endmodule: matvec3_part4
---
+++ part4/memory.sv
@@ -0,0 +1,19 @@
+module memory(clk, data_in, data_out, addr, wr_en);
+    parameter WIDTH=16, SIZE=64;
+    localparam LOGSIZE=$clog2(SIZE);
+    input [WIDTH-1:0] data_in;
+    output logic [WIDTH-1:0] data_out;
+    input [LOGSIZE-1:0] addr;
+    input clk, wr_en;
+
+    
+    logic [SIZE-1:0][WIDTH-1:0] mem;
+
+
+    always_ff @(posedge clk) begin
+        data_out <= mem[addr];
+        if (wr_en)
+            mem[addr] <= data_in;
+    end
+
+endmodule
